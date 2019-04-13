c6b696ecdf361e309e2d31d0e1aa13a676bb6f7b
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-04-13 07:37:27 +0200

    Add stats controller w/ by-category action

---
+++ app/Http/Controllers/StatsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Expense;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StatsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function byCategory()
+    {
+        $expenses = DB::table('expenses')
+            ->select('category', DB::raw('SUM(denomination) as total'))
+            ->groupBy('category')
+            ->get();
+
+
+        return response()->json([
+            'data' => $expenses,
+            'status' => 'success',
+        ]);
+    }
+}
