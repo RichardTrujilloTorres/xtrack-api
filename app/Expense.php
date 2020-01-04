4f627c11c261b29ea67296a59b7c5012aa0826db
Richard Trujillo <richard.trujillo.torres@gmail.com>
2020-01-04 05:11:58 +0100

    Add stats scopes

--- app/Expense.php
+++ app/Expense.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
 use Laravel\Scout\Searchable;
 
 class Expense extends Model
@@ -26,5 +28,73 @@ class Expense extends Model
     public function toSearchableArray()
     {
         return $this->toArray();
+    }
+
+    /**
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeByCategory($query)
+    {
+        return $query
+            ->select('category', DB::raw('SUM(denomination) as total'))
+            ->where(DB::raw('YEAR(created_at)'), DB::raw('YEAR(CURRENT_DATE())'))
+            ->groupBy('category')
+            ;
+    }
+
+    /**
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeByMonth($query)
+    {
+        return $query
+            ->select('category', DB::raw('SUM(denomination) as total'),DB::raw('MONTH(created_at) as month'))
+            ->where(DB::raw('YEAR(created_at)'), DB::raw('YEAR(CURRENT_DATE())'))
+            ->groupBy('category', 'month')
+            ;
+    }
+
+    /**
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeByDay($query)
+    {
+        return $query
+            ->select(DB::raw('DATE(created_at) as day'), DB::raw('SUM(denomination) as total'))
+            ->where(DB::raw('YEAR(created_at)'), DB::raw('YEAR(CURRENT_DATE())'))
+            ->groupBy('day')
+            ;
+    }
+
+    /**
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeHighest($query)
+    {
+            return $query
+                ->select('description', DB::raw('CAST(denomination AS DECIMAL(5,2)) as denomination'), 'category')
+                ->where(DB::raw('MONTH(created_at)'), DB::raw('MONTH(CURRENT_DATE())'))
+                ->where(DB::raw('YEAR(created_at)'), DB::raw('YEAR(CURRENT_DATE())'))
+                ->orderBy('denomination', 'desc')
+                ->limit(1)
+                ;
+    }
+
+    /**
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeHighestCategory($query)
+    {
+        return $query
+            ->select('category', DB::raw('SUM(denomination) as total'))
+            ->groupBy('category')
+            ->orderBy('total', 'desc')
+            ->limit(1)
+            ;
     }
 }
