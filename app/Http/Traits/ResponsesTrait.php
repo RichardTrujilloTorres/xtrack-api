56b4e8608aefc584b3e46406a9bd0e568ca187ad
Richard Trujillo <richard.trujillo.torres@gmail.com>
2020-01-11 06:51:37 +0100

    Add responses trait

---
+++ app/Http/Traits/ResponsesTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Traits;
+
+/**
+ * Trait ResponsesTrait
+ * @package App\Http\Traits
+ */
+trait ResponsesTrait
+{
+    /**
+     * @param $data
+     * @param int $code
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function success(
+        $data,
+        $code = 200,
+        $message = ''
+    ) {
+        return response()->json([
+            'data' => $data,
+            'status' => 'success',
+            'message' => $message,
+        ], $code);
+    }
+
+    /**
+     * @param array $data
+     * @param int $code
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function error(
+        $data = [],
+        $code = 400,
+        $message = 'An error occurred'
+    ) {
+        return response()->json([
+            'data' => $data,
+            'status' => 'error',
+            'message' => $message,
+        ], $code);
+    }
+}
